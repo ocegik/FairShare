0e409df6f0277fa6ecb6e1897f1738abd5804ef9
ocegik <147722344+ocegik@users.noreply.github.com>
2025-10-03 03:46:57 +0530

    Made Placeholder Screens
    Created Screens for testing and for future developments

---
+++ app/src/main/java/com/example/fairshare/ui/screens/ProfileScreen.kt
@@ -0,0 +1,24 @@
+package com.example.fairshare.ui.screens
+
+import androidx.navigation.NavController
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Profile Screen",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+
+}
