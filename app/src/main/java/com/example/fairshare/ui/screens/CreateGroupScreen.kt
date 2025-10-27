2ffae07655d992d1359acfc304e0f389e89b6b4f
ocegik <147722344+ocegik@users.noreply.github.com>
2025-10-27 17:27:58 +0530

    Create Group Creating Screen and pass Text Field

---
+++ app/src/main/java/com/example/fairshare/ui/screens/CreateGroupScreen.kt
@@ -0,0 +1,86 @@
+package com.example.fairshare.ui.screens
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.fairshare.ui.components.PasswordField
+import com.example.fairshare.ui.components.TitleField
+
+@Composable
+fun CreateGroupScreen(navController: NavController) {
+
+    var title by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var showError by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()), // allows scrolling if needed
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Create Room",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        TitleField(title){title = it}
+        Spacer(modifier = Modifier.height(24.dp))
+
+        PasswordField(
+            value = password,
+            onValueChange = {
+                password = it
+                showError = it.length < 6
+            },
+            isError = showError,
+            errorMessage = if (showError) "Password must be at least 6 characters" else null
+        )
+
+        Button(
+            onClick = {
+                Log.d("ExpenseForm", "=== SUBMIT BUTTON CLICKED ===")
+
+                val isValid = title.isNotBlank() && password.isNotBlank()
+
+
+                if (isValid) {
+
+                    //groupViewModel
+                    navController.popBackStack()
+                }
+                else {
+                    Log.e("GroupForm", "Validation failed - group not created")
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Text("Save Expense", fontSize = 16.sp)
+        }
+    }
+
+}
