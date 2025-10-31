33fb566744f5cbb63a05618b0fee5521210d60b5
ocegik <147722344+ocegik@users.noreply.github.com>
2025-10-31 13:15:01 +0530

    Update UserRepository.kt

--- app/src/main/java/com/example/fairshare/repository/UserRepository.kt
+++ app/src/main/java/com/example/fairshare/repository/UserRepository.kt
@@ -84,4 +84,110 @@ class UserRepository @Inject constructor(
     fun generateUserId(): String {
         return firestoreService.generateDocumentId(COLLECTION_PATH)
     }
+
+    // Subcollection operations
+    fun <T : Any> addToUserSubCollection(
+        userId: String,
+        subCollectionName: String,
+        documentId: String,
+        data: T,
+        onResult: (Boolean) -> Unit
+    ) {
+        Log.d(TAG, "Adding document to $subCollectionName for user: $userId")
+        val fullPath = "$COLLECTION_PATH/$userId/$subCollectionName"
+
+        firestoreService.addDocument(
+            collectionPath = fullPath,
+            documentId = documentId,
+            data = data,
+            onResult = { success ->
+                if (success) {
+                    Log.d(TAG, "Document added to $subCollectionName: $documentId")
+                } else {
+                    Log.e(TAG, "Error adding document to $subCollectionName")
+                }
+                onResult(success)
+            }
+        )
+    }
+
+    fun <T> getUserSubCollection(
+        userId: String,
+        subCollectionName: String,
+        clazz: Class<T>,
+        onResult: (List<T>) -> Unit
+    ) {
+        val fullPath = "$COLLECTION_PATH/$userId/$subCollectionName"
+
+        firestoreService.getAllDocuments(
+            collectionPath = fullPath,
+            clazz = clazz,
+            onResult = onResult
+        )
+    }
+
+    fun <T> getUserSubCollectionDocument(
+        userId: String,
+        subCollectionName: String,
+        documentId: String,
+        clazz: Class<T>,
+        onResult: (T?) -> Unit
+    ) {
+        val fullPath = "$COLLECTION_PATH/$userId/$subCollectionName"
+
+        firestoreService.getDocument(
+            collectionPath = fullPath,
+            documentId = documentId,
+            clazz = clazz,
+            onResult = onResult
+        )
+    }
+
+    fun updateUserSubCollectionDocument(
+        userId: String,
+        subCollectionName: String,
+        documentId: String,
+        updates: Map<String, Any>,
+        onResult: (Boolean) -> Unit
+    ) {
+        Log.d(TAG, "Updating document in $subCollectionName for user: $userId")
+        val fullPath = "$COLLECTION_PATH/$userId/$subCollectionName"
+
+        firestoreService.updateDocument(
+            collectionPath = fullPath,
+            documentId = documentId,
+            updates = updates,
+            onResult = { success ->
+                if (success) {
+                    Log.d(TAG, "Document updated in $subCollectionName: $documentId")
+                } else {
+                    Log.e(TAG, "Error updating document in $subCollectionName")
+                }
+                onResult(success)
+            }
+        )
+    }
+
+    fun deleteUserSubCollectionDocument(
+        userId: String,
+        subCollectionName: String,
+        documentId: String,
+        onResult: (Boolean) -> Unit
+    ) {
+        Log.d(TAG, "Deleting document from $subCollectionName for user: $userId")
+        val fullPath = "$COLLECTION_PATH/$userId/$subCollectionName"
+
+        firestoreService.deleteDocument(
+            collectionPath = fullPath,
+            documentId = documentId,
+            onResult = { success ->
+                if (success) {
+                    Log.d(TAG, "Document deleted from $subCollectionName: $documentId")
+                } else {
+                    Log.e(TAG, "Error deleting document from $subCollectionName")
+                }
+                onResult(success)
+            }
+        )
+    }
 }
